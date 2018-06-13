0c06b20a322e4f2ba83ab9ffc530f138d3945db4
Rohan Paul <rohanpaul2@gmail.com>
2018-06-13 21:58:36 +0530

    completing routes for student.js

--- routes/students.js
+++ routes/students.js
@@ -12,4 +12,24 @@ router.get('/show/:id', function(req, res) {
     student.show(req, res)
 })
 
-//
+//create a student
+router.get('/create', function(req, res) {
+    student.create(req, res);
+})
+
+// save student
+router.post('/save', function(req, res) {
+    student.save(req, res);
+})
+
+//Edit student
+router.get('/edit/:id', function(req, res) {
+    student.edit(req, res);
+});
+
+//Update a student
+router.post('/update/:id', function(req, res) {
+    student.update(req, res);
+})
+
+module.exports = router;
